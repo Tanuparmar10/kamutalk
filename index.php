eb6cd9283ff75048f5e0c39dca880165b1e46e9c
Kamutalk <Kamutalkindia@gmail.com>
2021-07-22 00:51:26 -0400

    first commit

--- index.php
+++ index.php
@@ -49,7 +49,53 @@
 
 <body data-spy="scroll" data-target=".navbar-nav" data-offset="110">
 
-<?php include('support_files/header.php'); ?>
+<!-- LOADER -->
+<div class="preloader">
+    <div class="loader_grid">
+      <div class="loader_box loader_box1"></div>
+      <div class="loader_box loader_box2"></div>
+      <div class="loader_box loader_box3"></div>
+      <div class="loader_box loader_box4"></div>
+      <div class="loader_box loader_box5"></div>
+      <div class="loader_box loader_box6"></div>
+      <div class="loader_box loader_box7"></div>
+      <div class="loader_box loader_box8"></div>
+      <div class="loader_box loader_box9"></div>
+    </div>
+</div>
+<!-- END LOADER --> 
+
+<!-- START HEADER -->
+<header class="header_wrap fixed-top light_skin hover_menu_style2 main_menu_capitalize main_menu_size_16 main_menu_weight_400 transparent-header">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg"> 
+        <a class="navbar-brand m-auto page-scroll" href="#home_section">
+            <img class="logo_light" src="demo-mobile-app/images/app-logo_white.png" alt="logo">
+            <img class="logo_dark" src="demo-mobile-app/images/app-logo-black.png" alt="logo">
+            <img class="logo_default" src="demo-mobile-app/images/app-logo_dark.png" alt="logo">
+        </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="ion-android-menu"></span> </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li>
+                        <a class="nav-link active page-scroll" href="#home_section">Home</a>
+                    </li>
+                    <li>
+                        <a class="nav-link page-scroll" href="#about">about</a>
+                    </li>
+                    <li>
+                        <a class="nav-link page-scroll" href="#services">Services</a>
+                    </li>
+                    <li>
+                        <a class="nav-link page-scroll" href="#pricing">Memberships</a>
+                    </li>
+                </ul>
+            </div>
+            <a href="#download" class="btn btn-outline-white btn-radius ml-lg-3 d-none d-sm-block nav-link page-scroll">Get The App</a>
+        </nav>
+    </div>
+</header>
+<!-- START HEADER --> 
 
 <!-- START SECTION BANNER -->
 <section id="home_section" class="banner_section bg_gd_moonpurple wave_shape_bottom overflow_hide">
@@ -382,7 +428,70 @@ to your friend and relative sitting in any corner of the world.</p>
         </div>
     </div>
 </section>
-<?php include('support_files/footer.php'); ?>
+
+<!-- START FOOTER SECTION --> 
+<footer class="bg_black3 footer_dark">
+  <div class="top_footer">
+        <div class="container">
+          <div class="row">
+                <div class="col-lg-4 col-md-12 animation" data-animation="fadeInUp" data-animation-delay="0.2s">
+                  <div class="footer_logo">
+                      <a href="#home_section"><img alt="logo" src="demo-mobile-app/images/app-logo_white.png"></a>
+                    </div>
+                    <p>Phasellus blandit massa enim. elit id varius nunc. Lorems ipsum dolor sit amet, consectetur industry.</p>
+                    <ul class="list_none footer_social">
+                      <li><a href="#"><i class="ion-social-facebook"></i></a></li>
+                        <li><a href="#"><i class="ion-social-twitter"></i></a></li>
+                        <li><a href="#"><i class="ion-social-googleplus"></i></a></li>
+                        <li><a href="#"><i class="ion-social-youtube-outline"></i></a></li>
+                        <li><a href="#"><i class="ion-social-instagram-outline"></i></a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-6 mt-4 mt-lg-0 animation" data-animation="fadeInUp" data-animation-delay="0.3s">
+                  <h6 class="widget_title">USEFUL LINKS</h6>
+                    <ul class="list_none widget_links_style1">
+                        <li><a href="#">How it Works</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Pricing</a></li>
+                        <li><a href="#">Testimonial</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Term Conditions</a></li>
+                        <li><a href="privacypolicy.html">Privacy Policy</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-6 mt-4 mt-lg-0 animation" data-animation="fadeInUp" data-animation-delay="0.5s">
+                    <h6 class="widget_title">Subscribe Newsletter</h6>
+                    <p>Contrary to popular belief of lorem Ipsm Latin amet ltin from consectetur industry.</p>
+                    <div class="newsletter_form">
+                        <form> 
+                            <div class="rounded_input">
+                              <input type="text" required="" placeholder="Enter Email Address">
+                            </div>
+                            <button type="submit" title="Subscribe" class="btn btn-default btn-radius" name="submit" value="Submit">
+                              subscribe
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+          <div class="row align-items-center">
+              <div class="col-md-12">
+                  <div class="bottom_footer border_top_tran">
+                      <div class="row">
+                          <div class="col-md-12">
+                                <p class="copyright m-md-0 text-center">© 2019 All Rights Reserved by Codegenius.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</footer>
+<!-- END FOOTER SECTION --> 
 
 <a href="#" class="scrollup" style="display: none;"><i class="ion-ios-arrow-up"></i></a> 
 
